e4e2572acb1b4b3f8e5afca7c2188c947ea3ee17
Welber <serafimwelber@gmail.com>
2024-11-30 20:49:34 -0300

    initial commit

---
+++ src/main/java/br/com/webflux/CrashServiceForLearningWebfluxApplication.java
@@ -0,0 +1,13 @@
+package br.com.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrashServiceForLearningWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrashServiceForLearningWebfluxApplication.class, args);
+	}
+
+}
